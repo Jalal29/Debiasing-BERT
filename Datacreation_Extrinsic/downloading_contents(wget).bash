28c5a810f6b164cd9bc9f3a28fccc7b94169ec32
Jalal29 <70238972+Jalal29@users.noreply.github.com>
2021-12-12 06:47:29 +0530

    Add files via upload

---
+++ Datacreation_Extrinsic/downloading_contents(wget).bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+input="listofURL_newForwget_8740.txt"
+c=0
+while IFS= read -r line
+do
+c=`expr $c + 1`
+echo "fetching from $line and storing in $c.html"
+wget $line -O $c.html -T 30 -t 2
+done < "$input"
